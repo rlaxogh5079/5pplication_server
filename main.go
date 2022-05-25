343cd2d2d27af1fac30eb7c41acf6c049ceb032d
JayKim0202 <jayjkim0223@gmail.com>
2022-05-25 15:06:06 +0900

    test

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"github.com/5pplication/Server/db"
-	"github.com/5pplication/Server/login"
-	"golang.org/x/crypto/bcrypt"
-	"fmt"
+	"github.com/5pplication/Server/rest"
 )
 
+/*
 func main() {
 	insertPassword, hashErr := bcrypt.GenerateFromPassword([]byte("테스트"), bcrypt.DefaultCost)
 	insertErr := db.InsertUser("테스트", "테스트", "테스트", string(insertPassword))
 	fmt.Println(hashErr, insertErr)
 	login.Login()
+}
+*/
+
+func main() {
+	rest.Api().Run()
 }
