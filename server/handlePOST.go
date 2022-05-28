403315835a0a3f59dffac7312e2ed3a0371c47a1
rlaxogh5079 <rlaxogh507906@gmail.com>
2022-05-29 05:40:10 +0900

    :sparkles: 회원가입

--- server/handlePOST.go
+++ server/handlePOST.go
@@ -12,23 +12,25 @@ import (
 func POSTLogin(c *gin.Context) {
 	c.Header("Content-Type", "application/json charset=utf-8")
 	email := c.Request.Header["Email"][0]
-	fmt.Println(email)
+	fmt.Printf("%v님이 접속을 시도합니다.\n", email)
 	password := []byte(c.Request.Header["Password"][0])
 	result, loginErr := login.CheckLogin(email, password)
-	if loginErr != nil {
-		c.JSON(http.StatusInternalServerError, gin.H {
-			"message" : loginErr,
+	if result == -1 {
+		c.JSON(http.StatusUnauthorized, gin.H {
+			"error" : loginErr.Error(),
+			"message": "아이디가 존재하지 않습니다.",
 		})
-	}
-	fmt.Println(result)
-	if result == 0 { // 비밀번호가 틀림
+		fmt.Println("아이디가 존재하지 않습니다.")
+	} else if result == 0 { // 비밀번호가 틀림
 		c.JSON(http.StatusUnauthorized, gin.H{
 			"message": "비밀번호가 틀렸습니다.",
 	})
+		fmt.Println("비밀번호가 틀렸습니다.")
 	} else { // 로그인 성공
 		c.JSON(http.StatusOK, gin.H {
 			"message" : "환영합니다.",
 		})
+		fmt.Println("환영합니다.")
 	}
 }
 
@@ -41,9 +43,13 @@ func POSTSignUp(c* gin.Context) {
 	insertErr := database.InsertUser(email, nickname, password, storeArticle)
 	if insertErr != nil {
 		c.JSON(http.StatusInternalServerError, gin.H {
-			"message" : insertErr,
+			"error" : insertErr.Error(),
+			"message" : "해당 이메일은 이미 가입되었습니다.",
 		})
 	} else {
-		c.String(http.StatusOK, "회원가입 성공")
+		c.JSON(http.StatusOK, gin.H{
+			"message": "회원가입 성공",
+		})
+		fmt.Printf("%v님이 회원가입 하였습니다.\n", email)
 	}
 }
