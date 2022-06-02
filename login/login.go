b07a4f9bb0f8155d7a77ba5c49a2f6772c239481
rlaxogh5079 <rlaxogh507906@gmail.com>
2022-06-03 03:10:16 +0900

    :zap: CheckLogin 함수 간편화

--- login/login.go
+++ login/login.go
@@ -7,14 +7,14 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func CheckLogin(userEmail string, userPassword []byte) (int, error) { // 로그인 성공 -> 1, 로그인 실패 -> 0, 아이디 존재 X -> -1
+func CheckLogin(userEmail string, userPassword string) (int, error) { // 로그인 성공 -> 1, 로그인 실패 -> 0, 아이디 존재 X -> -1
 	user, selectErr := database.SelectUser(userEmail)
 	if selectErr != nil {
 		fmt.Println(selectErr.Error())
 	}
 
 	if user.Email == userEmail {
-		compareErr := bcrypt.CompareHashAndPassword([]byte(user.Password), userPassword)
+		compareErr := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(userPassword))
 		if compareErr != nil {
 			return 0, selectErr
 		} else {
